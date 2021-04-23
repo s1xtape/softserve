f777e9b33cf3f08bd35b9c107a4f74ff98b6fb20
Maksim Gorbenko <gorbenko.m21@cf.dnu.dp.ua>
2021-04-23 16:19:16 +0300

    docs: add HomeWrok5

--- Lab of js/app/HW5/lab5.js
+++ Lab of js/app/HW5/lab5.js
@@ -54,71 +54,7 @@ function toQuipu(result){
     return resultStr;
 }
 //Task 1 end
-//Task 2 start 
-let maxA = [];
-function findMaxProd (num){
-    maxA = [num,num];
-    let arr = [num,0];
-    let iter = num;
-    while (iter != 1) {
-            console.log("opachi");
-            arr.splice(1)
-            arr[0]--;
-            iter = arr[0];
-            arr[1] = num - arr[0];
-            findMax(arr);
-            if(arr[1] > 2){
-            checkArr(arr,1);
-            }else{
-                if(arr[1] == 2){
-                    arr.splice(1);
-                    arr.push(1);
-                    arr.push(1);
-                }
-            }
-            findMax(arr);
-    }
-    console.log(arr + "   final");
-    console.log(maxA);
-    alert(maxA.join(' '));
-}
 
-function checkArr(arr,i){
-    let temp = arr[i];
-    while (arr[i] !=1 ) {
-            arr.splice(i+1)
-            arr[i]--;
-            arr[i+1] = temp - arr[i];
-            findMax(arr);
-            if(arr[i+1] > 2){
-            checkArr(arr,i+1);
-            }else{
-                if(arr[i+1] == 2){
-                    arr.splice(i+1);
-                    arr.push(1);
-                    arr.push(1);
-                }
-            }
-           findMax(arr);
-    }
-}
-function findMax(arr){
-    let m = 1;
-    let a = new Array();
-    arr.forEach(element => {
-        m *= element;        
-    });
-    if(m > maxA[0]){
-        maxA[0] = m;
-        maxA.splice(1);
-        maxA.push(arr.join(','));
-    }
-    if(m == maxA[0]){
-        console.log(arr);
-        maxA.push(arr.join(','));
-    }
-}
-//Task 2 end
 // Task 3 start
 
 function clerk(arr){
